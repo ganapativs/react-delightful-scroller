30f821ec485406a3d07c3fabeec645acc7530a7e
Ganapati V S <vsg.inbox@gmail.com>
2019-07-27 15:08:08 +0530

    Throttle scroll with requestAnimationFrame

--- src/iscroller/useScroll.js
+++ src/iscroller/useScroll.js
@@ -1,4 +1,5 @@
 import { useEffect, useRef, useState } from 'react';
+import throttle from 'lodash.throttle';
 
 const getScrollOffset = (element, axis) => {
   const { scrollTop, scrollY } = element;
@@ -6,30 +7,32 @@ const getScrollOffset = (element, axis) => {
 };
 
 export const useScroll = ({ root, axis }) => {
-  const element = root || window;
   const timeout = useRef(null);
   const [scrollOffset, setScrollOffset] = useState(0);
 
   useEffect(() => {
-    const handler = () => {
+    const element = root || window;
+    const handler = throttle(() => {
       // If there's a timer, cancel it
       if (timeout.current) {
         window.cancelAnimationFrame(timeout.current);
       }
-      // Setup the new requestAnimationFrame()
+      // Setup the requestAnimationFrame
       timeout.current = window.requestAnimationFrame(() =>
         setScrollOffset(getScrollOffset(element, axis)),
       );
-    };
+    }, 100);
     element.addEventListener('scroll', handler, {
       capture: false,
       passive: true,
     });
+
     return () => {
+      handler.cancel && handler.cancel();
       window.cancelAnimationFrame(timeout.current);
       element.removeEventListener('scroll', handler);
     };
-  }, [axis, element]);
+  }, [axis, root]);
 
   return scrollOffset;
 };
