819ec72bfba60b1bbf83240b25bc5b2a405056ce
Ganapati V S <vsg.inbox@gmail.com>
2019-09-13 19:46:51 +0530

    Typo fix

--- packages/react-delightful-scroller/src/getBatchedItems.js
+++ packages/react-delightful-scroller/src/getBatchedItems.js
@@ -1,7 +1,7 @@
 export const getBatchedItems = (items, batchSize = 1) => {
   const batched = [];
 
-  // Faster that clone and splice
+  // Faster than clone and splice
   for (let index = 0; index < items.length; index += batchSize) {
     const chunk = items.slice(index, index + batchSize);
     // Do something if you want with the group
